ccd3705ad07adf937e2da0f55e8e270a869c5d79
= <debelen.arthurreiner@gmail.com>
2022-11-06 16:44:57 -0500

    possibly working?

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Faster CSV parsing in Python
+----
+Arthur:
+- Debian GNU/Linux 11
+- Python 3.9
+- installed packages python3.9-dev and libpython3.9-dev
+
+
+To convert `sievecsvmodule.c` to `libsievecsv.so`:
+- `gcc $(pkg-config --cflags --libs python3) -Wall -shared -o libsievecsv.so -fPIC sievecsvmodule.c`
+
+To test out the contents of `sievecsvmodule.c`:
+- Open up Python REPL.
+- `from ctypes import *`
+- `import os`
+- `cdll.LoadLibrary(os.path.abspath("libsievecsv.so")`
+- `spamLib = CDLL(os.path.abspath("libsievecsv.so"))`
+- `spamLib.system`
+- `spamLib.system("ls -la")`
--- sievecsvmodule.c
+++ sievecsvmodule.c
@@ -1,67 +1,57 @@
-#include <stdio.h>
-void foo() {
-    printf("ding dong");
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject*
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+    if (!PyArg_ParseTuple(args, "s", &command)) {
+        return NULL;
+    }
+    sts = system(command);
+    return PyLong_FromLong(sts);
 }
 
-// #define PY_SSIZE_T_CLEAN
-// #include <Python.h>
+static PyMethodDef SpamMethods[] = {
+    {"system", spam_system, METH_VARARGS, "Execute a shell command."},
+    {NULL, NULL, 0, NULL}
+};
 
+PyDoc_STRVAR(spam_doc, "Module named spam");
+static struct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT,
+    "spam",
+    spam_doc,
+    -1,
+    SpamMethods
+};
 
-// static PyObject*
-// sievecsv_yeet(PyObject *self, PyObject *args) {
-//     printf("what the fuck");
-//     return PyLong_FromLong(20);
-// }
+PyMODINIT_FUNC
+PyInit_spam(void) {
+    return PyModule_Create(&spammodule);
+}
 
-// static PyMethodDef SieveCSVMethods[] = {
-//     {"yeet", sievecsv_yeet, METH_VARARGS, "Yeet"},
-//     {NULL, NULL, 0, NULL}
-// };
+int
+main(int argc, char *argv[]) 
+{
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+    if(PyImport_AppendInittab("spam", PyInit_spam) == -1) {
+        fprintf(stderr, "Error: could not extend in-built modules table\n");
+        exit(1);
+    }
 
-// static struct PyModuleDef sievecsvmodule = {
-//     PyModuleDef_HEAD_INIT,
-//     "sievecsv",
-//     NULL, // we don't have documentation.... yet. 
-//     -1,
-//     SieveCSVMethods
-// }
-
-// PyMODINIT_FUNC
-// PyInit_sievecsv(void)
-// {
-//     return PyModule_Create(&sievecsvmodule);
-// }
-
-// int 
-// main(int argc, char *argv[]) {
-//     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
-//     if (program == NULL) {
-//         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-//         exit(1);
-//     }
-
-//     /* Add a built-in module, before Py_Initialize */
-//     if (PyImport_AppendInittab("sievecsv", PyInit_sievecsv) == -1) {
-//         fprintf(stderr, "Error: could not extend in-built modules table\n");
-//         exit(1);
-//     }
-
-//     /* Pass argv[0] to the Python interpreter */
-//     Py_SetProgramName(program);
-
-//     /* Initialize the Python interpreter.  Required.
-//        If this step fails, it will be a fatal error. */
-//     Py_Initialize();
-
-//     /* Optionally import the module; alternatively,
-//        import can be deferred until the embedded script
-//        imports it. */
-//     PyObject *pmodule = PyImport_ImportModule("sievecsv");
-//     if (!pmodule) {
-//         PyErr_Print();
-//         fprintf(stderr, "Error: could not import module 'sievecsv'\n");
-//     }
-
-//     PyMem_RawFree(program);
-//     return 0;
-// }
+    Py_SetProgramName(program);
+    Py_Initialize();
+    PyObject *pmodule = PyImport_ImportModule("spam");
+    if (!pmodule) {
+        PyErr_Print();
+        fprintf(stderr, "Error: could not import module 'spam'\n");
+    }
+    PyMem_RawFree(program);
+    return 0;
+}
