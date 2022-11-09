7c74c7a376ac466f0aee8e1079b92a47ebbf49ac
austinjstromme <austinjstromme@gmail.com>
2022-11-09 11:51:57 -0500

    No need for main method

--- sievecsvmodule.c
+++ sievecsvmodule.c
@@ -32,26 +32,26 @@ PyInit_SieveCSV(void) {
     return PyModule_Create(&SieveCSVmodule);
 }
 
-int
-main(int argc, char *argv[]) 
-{
-    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-        exit(1);
-    }
-    if(PyImport_AppendInittab("SieveCSV", PyInit_SieveCSV) == -1) {
-        fprintf(stderr, "Error: could not extend in-built modules table\n");
-        exit(1);
-    }
-
-    Py_SetProgramName(program);
-    Py_Initialize();
-    PyObject *pmodule = PyImport_ImportModule("SieveCSV");
-    if (!pmodule) {
-        PyErr_Print();
-        fprintf(stderr, "Error: could not import module 'SieveCSV'\n");
-    }
-    PyMem_RawFree(program);
-    return 0;
-}
+//int
+//main(int argc, char *argv[]) 
+//{
+//    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+//    if (program == NULL) {
+//        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+//        exit(1);
+//    }
+//    if(PyImport_AppendInittab("SieveCSV", PyInit_SieveCSV) == -1) {
+//        fprintf(stderr, "Error: could not extend in-built modules table\n");
+//        exit(1);
+//    }
+//
+//    Py_SetProgramName(program);
+//    Py_Initialize();
+//    PyObject *pmodule = PyImport_ImportModule("SieveCSV");
+//    if (!pmodule) {
+//        PyErr_Print();
+//        fprintf(stderr, "Error: could not import module 'SieveCSV'\n");
+//    }
+//    PyMem_RawFree(program);
+//   return 0;
+//}
