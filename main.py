a3e767f25258a26ed10dd1f4444f0b4f9aa1c0c6
austinjstromme <austinjstromme@gmail.com>
2022-11-14 23:34:07 -0500

    Removed unused files

--- main.py
+++
@@ -1,6 +0,0 @@
-import SieveCSV
-
-collection = SieveCSV.parse_csv("random.csv", [1, 2, 3], ["bab", "abba", "abacaba"])
-print(collection)
-# collection = SieveCSV.parse_csv("random.csv", [(1, "bab"), (2, "abba"), (3, "abacaba")])
-# collecction = SieveCSV.parse_csv("random.csv", 1, "bab", 2, "abba", 3, "abacaba")
