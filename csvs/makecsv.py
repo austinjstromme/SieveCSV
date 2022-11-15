a8ba4f896a07bc0ee086b1f88017555eec414fcd
austinjstromme <austinjstromme@gmail.com>
2022-11-14 23:35:23 -0500

    Added params

--- csvs/makecsv.py
+++ csvs/makecsv.py
@@ -3,7 +3,7 @@ import numpy as np
 
 WIDTH = 100
 LENGTH = 10000
-RANGE = 999
+RANGE = 100
 
 np.random.seed(0)
 
@@ -11,5 +11,5 @@ with open("largecsvrandom.csv", "w", newline='') as f:
     writer = csv.writer(f)
 
     for l in range(0, LENGTH):
-        writer.writerow([np.random.randint(100) for j in range(0, WIDTH)])
+        writer.writerow([np.random.randint(RANGE) for j in range(0, WIDTH)])
 
